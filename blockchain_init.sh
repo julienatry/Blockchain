fd5dda63fbf70e093934163712f361faf703a286
Julien Atry <atryjulien@gmail.com>
2022-02-10 17:12:43 +0100

    Update blockchain_init.sh
    Added privileges verification

--- blockchain_init.sh
+++ blockchain_init.sh
@@ -4,9 +4,20 @@
 sharedPubKey="/mnt/pubkey/"
 networkAddress="192.168.80.0/24"
 
+
+
+#Verify root privileges
+if [[ $EUID -ne 0 ]]; then
+   echo "I must be opened by root"
+   exit 1
+fi
+
+
+
 #Variables
 randomValue=$RANDOM
 nmap_output=$(nmap $1 -n -sP $networkAddress | grep report | awk '{print $5}')
+
 
 
 #Verbose
@@ -20,7 +31,7 @@ echo "----------------"
 mkdir $sharedPubKey
 chmod 777 $sharedPubKey
 
-echo "/mnt/pubkey $networkAddress(ro,sync,no_subtree_check)" >> /etc/exports
+echo "/mnt/pubkey $networkAddress(ro,sync,no_subtree_check)" > /etc/exports
 
 exportfs -a
 systemctl restart nfs-kernel-server
