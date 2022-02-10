f40402dd0fbf892cb0e12dbf4dab118bba3ddaa4
Julien Atry <atryjulien@gmail.com>
2022-02-10 15:41:05 +0100

    Added comments

--- blockchain_init.sh
+++ blockchain_init.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+#Config
 sharedPubKey="/mnt/pubkey/"
 networkAddress="192.168.80.0/24"
 
-
-
+#Variables
 randomValue=$RANDOM
 nmap_output=$(nmap $1 -n -sP $networkAddress | grep report | awk '{print $5}')
 
 
 
+
+#NFS share
 mkdir $sharedPubKey
 chmod 777 $sharedPubKey
 
@@ -17,11 +19,11 @@ echo "/mnt/pubkey $networkAddress(ro,sync,no_subtree_check)" >> /etc/exports
 
 exportfs -a
 systemctl restart nfs-kernel-server
+echo "----------------"
 
 
 
-
-
+#DSH hosts list
 echo "Live blockchain hosts :"
 for i in $nmap_output
 do
@@ -35,6 +37,8 @@ cat /etc/dsh/group/blockchain >> /etc/dsh/machines.list
 echo "----------------"
 
 
+
+#Public SSH keys retrieving
 for i in $nmap_output
 do
    if [ "${i##*.}" -gt "100" ] && [ "${i##*.}" -lt "200" ]
