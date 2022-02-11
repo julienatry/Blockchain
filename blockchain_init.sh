45d020f0a1df7398b6adebd4c0e8829e066760a2
Julien Atry <atryjulien@gmail.com>
2022-02-11 11:50:21 +0100

    Bug patches

--- blockchain_init.sh
+++ blockchain_init.sh
@@ -48,9 +48,9 @@ echo "----------------"
 #Creating SSH keys
 if [ -f $rsa_file ]; then
    rm $rsa_file $rsa_file.pub
-else
-   ssh-keygen -f $rsa_file -N "" -t rsa
 fi
+
+ssh-keygen -f $rsa_file -N "" -t rsa
 
 cp $rsa_file.pub $sharedPubKey
 
