23e3cfb5252cb8f12cde8f14a2952260d578c98e
Julien Atry <atryjulien@gmail.com>
2022-02-14 17:57:46 +0100

    Bug fix

--- blockchain_init.sh
+++ blockchain_init.sh
@@ -80,7 +80,7 @@ fi
 if [[ -z $isDSHConfigured ]]; then
    sed -i 's/remoteshell =rsh/remoteshell =ssh/g' $dsh_config
 
-   echo "#Configured for blockchain" $dsh_config
+   echo "#Configured for blockchain" >> $dsh_config
 fi
 
 
