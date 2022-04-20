0175c675111303d60c872e4c30df6c25343a7040
Julien Atry <atryjulien@gmail.com>
2022-04-20 17:18:57 +0200

    Update blockchain_new_device.sh
    Removed useless if

--- blockchain_new_device.sh
+++ blockchain_new_device.sh
@@ -37,13 +37,11 @@ dsh_update () {
 }
 
 ssh_update () {
-	if [[ -z $known_hosts_exists ]]; then
-		sed -i "/$1/d" ~/.ssh/known_hosts
-		sshKeyScan=$(ssh-keyscan -t rsa $1)
+	sed -i "/$1/d" ~/.ssh/known_hosts
+	sshKeyScan=$(ssh-keyscan -t rsa $1)
 
-		if [[ -z $known_hosts_rsa ]]; then
-			echo $sshKeyScan >> ~/.ssh/known_hosts
-		fi
+	if [[ -z $known_hosts_rsa ]]; then
+		echo $sshKeyScan >> ~/.ssh/known_hosts
 	fi
 }
 
