da31b211c9924a3e079402fd6fe50f051b340683
Julien Atry <atryjulien@gmail.com>
2022-04-21 11:38:13 +0200

    Update blockchain_new_device.sh

--- blockchain_new_device.sh
+++ blockchain_new_device.sh
@@ -38,7 +38,7 @@ dsh_update () {
 
 ssh_update () {
 	if [[ ! -z known_hosts_exists ]]; then
-		sshKeyScan=$(ssh-keyscan -t rsa $1)
+		sshKeyScan=$(ssh-keyscan -H -t rsa $1)
 
 		if [[ ! -z known_hosts_rsa ]]; then
 			sed -i "/$1/d" ~/.ssh/known_hosts
