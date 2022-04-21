7535d2f8ff15b183da8e201667f7ece50ce996cc
Julien Atry <atryjulien@gmail.com>
2022-04-21 13:44:29 +0200

    Update blockchain_new_device.sh

--- blockchain_new_device.sh
+++ blockchain_new_device.sh
@@ -52,7 +52,9 @@ ssh_update () {
 			;;
 
 		authorized_keys )
+			mount -t nfs $ip:/mnt/pubkey $pubkey_dir
 			cat $pubkey_dir/id_rsa.pub >> ~/.ssh/authorized_keys
+			umount $pubkey_dir
 			;;
 
 		reload )
@@ -89,10 +91,8 @@ do
 				mkdir $pubkey_dir
 			fi
 
-			mount -t nfs $ip:/mnt/pubkey $pubkey_dir
 			ssh_update authorized_keys
 			ssh_update reload
-			umount $pubkey_dir
 		fi
 	done
 	sleep 60
