c8de8f34a43114eb7d09fbf445244946afb611b7
Julien Atry <atryjulien@gmail.com>
2022-06-02 15:10:46 +0200

    Created install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt update && sudo apt install -y openssh-server dsh nmap nfs-kernel-server nfs-common net-tools
+
+git clone https://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@github.com/TakaFrey/Blockchain.git
+
+mv Blockchain/blockchain_* /root/scripts
+mv Blockchain/blacklist_ip /root/scripts
+mv Blockchain/services/* /etc/systemd/system/
+
+chmod +x /root/scripts/blockchain_*
+
+
+systemctl enable blockchain
+systemctl enable blockchain_new_device
+systemctl enable blockchain_passwd_check
