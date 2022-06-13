7d5b826e5a45ee274294adbfc39aa740ee8961af
Julien Atry <57760772+julienatry@users.noreply.github.com>
2022-06-13 10:07:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 sudo apt update && sudo apt install -y openssh-server dsh nmap nfs-kernel-server nfs-common net-tools
 
-git clone https://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@github.com/TakaFrey/Blockchain.git
+git clone https://github.com/julienatry/Blockchain.git
 
 mv Blockchain/blockchain_* Blockchain/blacklist_ip /root/scripts
 mv Blockchain/services/* /etc/systemd/system/
